e3c5130aaed2837cd2e05da84b326d9212a6a1b2
Victor Agudelo <vmagudeloj@gmail.com>
2023-01-19 16:08:11 -0500

    SE AGREGA UN CICLO FOR

--- PROYECTO.py
+++ PROYECTO.py
@@ -6,3 +6,14 @@ Created on Thu Jan 19 14:05:01 2023
 """
 
 #ESTOY EMPEZANDO CON GIT / GITHUB PRIMERA VERSION 
+#SE IMPORTAN LIBRERIAS EN PROYECTO
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#SE AGREGA UN CICLO
+a=0
+for i in range(5):
+    a+=1
+    print(a)
+
